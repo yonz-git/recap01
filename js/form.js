f38a8342f6916624845068198611ab0bba63db3d
Yeon Ji Lee <yonzelle@naver.com>
2026-03-28 03:09:51 +0100

    new tag

--- js/form.js
+++ js/form.js
@@ -5,45 +5,47 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const newCardContainer = document.createElement("section");
-  const newCard = document.createElement("article");
-  const question = document.createElement("h2");
-  const answer = document.createElement("p");
-  const btnShowAnswer = document.createElement("button");
-  const showAnswer = document.createElement("span");
-  const tag = document.createElement("ul");
-
-  const formElements = event.target.elements;
-  const questionData = formElements.question.value;
-  const answerData = formElements.answer.value;
-
-  const tagData = formElements.tag.value;
-
   document.getElementById("formContent").append(newCardContainer);
-  newCard.classList.add("newCard");
 
+  const newCard = document.createElement("article");
   newCardContainer.append(newCard);
-  newCard.append(question, answer, btnShowAnswer);
+
+  const question = document.createElement("h3");
+  const newAnswer = document.createElement("h2");
+
+  newCard.append(question, newAnswer);
+
+  const btnShowAnswer = document.createElement("button");
+  newAnswer.append(btnShowAnswer);
+
+  const showAnswer = document.createElement("span");
   btnShowAnswer.append(showAnswer);
-  showAnswer.append(tag);
+  showAnswer.textContent = "Show Answer";
+
+  const tagList = document.createElement("ul");
+  newCard.append(tagList);
+
+  const tag = document.createElement("li");
+
+  tagList.append(tag);
+
+  newCardContainer.classList.add("newCardContainer");
+  newCard.classList.add("newCard");
+  question.classList.add("question");
+  newAnswer.classList.add("newAnswer");
+  btnShowAnswer.classList.add("btnShowAnswer");
+  showAnswer.classList.add("showAnswer");
+  tagList.classList.add("tagList");
+  tag.classList.add("tag");
 
   //DATA collection
 
+  const formElements = event.target.elements;
+  const questionData = formElements.question.value;
+  const newAnswerData = formElements.newAnswer.value;
+  const tagData = formElements.tag.value;
+
   question.textContent = questionData;
-  answer.textContent = answerData;
+  newAnswer.textContent = newAnswerData;
   tag.textContent = tagData;
-
-  console.log(formElements.question);
-  console.log(formElements.question.value);
-
-  console.log(formElements.answer);
-  console.log(formElements.answer.value);
-
-  console.log(formElements.tag);
-  console.log(formElements.tag.value);
 });
-
-// const userInput = document.querySelector("formElements").value;
-
-// userInput = document.addEventListener("input", (event) => {
-//   console.log(event.target.value);
-// });
